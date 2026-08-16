6b38bbfb7e19229f104c0af539d5c8e6598c2ee1
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 14:23:40 -0300

    feat: cria lista

--- src/exercicio03.rb
+++ src/exercicio03.rb
@@ -6,3 +6,17 @@ class NoMatricial
     @elo = elo
   end
 end
+
+lista = Array.new(8)
+
+lista[2] = NoMatricial.new("N", 5)
+lista[5] = NoMatricial.new("I", 6)
+lista[6] = NoMatricial.new("C", 7)
+lista[7] = NoMatricial.new("E", 0)
+
+lista[3] = NoMatricial.new("T", 1)
+lista[1] = NoMatricial.new("R", 4)
+lista[4] = NoMatricial.new("Y", 0)
+
+comeco_a = 2
+comeco_b = 3
