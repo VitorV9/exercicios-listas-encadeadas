a88495bada86bfb5152891eecb0b42a5d6086f99
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 14:33:29 -0300

    feat: cria função para identificar letra procurada

--- src/exercicio03.rb
+++ src/exercicio03.rb
@@ -9,14 +9,30 @@ end
 
 lista = Array.new(8)
 
+#A
 lista[2] = NoMatricial.new("N", 5)
 lista[5] = NoMatricial.new("I", 6)
 lista[6] = NoMatricial.new("C", 7)
 lista[7] = NoMatricial.new("E", 0)
 
+#B
 lista[3] = NoMatricial.new("T", 1)
 lista[1] = NoMatricial.new("R", 4)
 lista[4] = NoMatricial.new("Y", 0)
 
-comeco_a = 2
-comeco_b = 3
+comeco = 2
+
+def existe_na_lista(lista, comeco, letra_procurada)
+  posicao = comeco
+
+  while posicao != 0
+    if lista[posicao].info == letra_procurada
+      return true
+
+      else
+      posicao = lista[posicao].elo
+    end
+  end
+
+  false
+end
