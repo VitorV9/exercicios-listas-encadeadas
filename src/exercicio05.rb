b44cf5f588ff7145951247f7ce66ee8a8357f516
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 16:18:05 -0300

    feat: cria função para inverter lista

--- src/exercicio05.rb
+++ src/exercicio05.rb
@@ -6,3 +6,41 @@ class No
     @elo = elo
   end
 end
+
+def inverter_lista(comeco)
+  anterior = nil
+  atual = comeco
+
+  while atual != nil
+    proximo = atual.elo
+    atual.elo = anterior
+    anterior = atual
+    atual = proximo
+  end
+
+  anterior
+end
+
+gaveta_c = No.new("C", nil)
+gaveta_b = No.new("B", gaveta_c)
+gaveta_a = No.new("A", gaveta_b)
+comeco = gaveta_a
+
+puts "LISTA ORIGINAL"
+posicao = comeco
+
+  while posicao != nil
+    print "#{posicao.info} | "
+    posicao = posicao.elo
+  end
+
+novo_comeco = inverter_lista(comeco)
+puts ""
+puts ""
+puts "LISTA INVERTIDA"
+posicao = novo_comeco
+
+  while posicao != nil
+    print "#{posicao.info} | "
+    posicao = posicao.elo
+  end
