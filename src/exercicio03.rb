1f3b5df4b1cb39fc545a662ecb3babab9e5d8161
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 14:55:17 -0300

    feat: cria para checar primeiro elemento exclusivo

--- src/exercicio03.rb
+++ src/exercicio03.rb
@@ -16,19 +16,19 @@ lista[6] = NoMatricial.new("C", 7)
 lista[7] = NoMatricial.new("E", 0)
 
 #B
-lista[3] = NoMatricial.new("T", 1)
-lista[1] = NoMatricial.new("R", 4)
-lista[4] = NoMatricial.new("Y", 0)
+lista[3] = NoMatricial.new("I", 1)
+lista[1] = NoMatricial.new("C", 4)
+lista[4] = NoMatricial.new("E", 0)
 
-comeco = 2
+comeco_a = 2
+comeco_b = 3
 
-def existe_na_lista(lista, comeco, letra_procurada)
-  posicao = comeco
+def existe_na_lista(lista, comeco_b, letra_procurada)
+  posicao = comeco_b
 
   while posicao != 0
     if lista[posicao].info == letra_procurada
       return true
-
       else
       posicao = lista[posicao].elo
     end
@@ -36,3 +36,25 @@ def existe_na_lista(lista, comeco, letra_procurada)
 
   false
 end
+
+def primeiro_exclusivo(lista, comeco_a, comeco_b)
+  posicao_a = comeco_a
+
+  while posicao_a != 0
+    letra_atual = lista[posicao_a].info
+
+    achou_no_b = existe_na_lista(lista, comeco_b, letra_atual)
+
+    if achou_no_b == false
+      return letra_atual
+      else
+      posicao_a = lista[posicao_a].elo
+    end
+  end
+
+  "Nenhuma letra exclusiva encontrada."
+end
+
+resultado = primeiro_exclusivo(lista, comeco_a, comeco_b)
+
+puts "A primeira letra exclusiva da Lista A é: #{resultado}"
