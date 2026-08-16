d73bb189e92c9a18ba7be02f40521819e91aff0c
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 16:51:14 -0300

    feat: cria listas e laço para apresentar concatenação

--- src/exercicio07.rb
+++ src/exercicio07.rb
@@ -20,3 +20,25 @@ def concatenar_lde(comeco_a, comeco_b)
 
   comeco_a
 end
+
+a1 = NoDuplo.new(10)
+a2 = NoDuplo.new(20)
+a1.prox = a2
+a2.ant = a1
+comeco_a = a1
+
+b1 = NoDuplo.new(30)
+b2 = NoDuplo.new(40)
+b1.prox = b2
+b2.ant = b1
+comeco_b = b1
+
+nova_lista = concatenar_lde(comeco_a, comeco_b)
+
+puts "LISTA CONCATENADA"
+posicao = nova_lista
+
+while posicao != nil
+  print " #{posicao.info} |"
+  posicao = posicao.prox
+end
