b14d9eed5cf39d9340ef60f13c7a8862e5bf6da6
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 16:45:58 -0300

    feat: cria função para concatenar listas

--- src/exercicio07.rb
+++ src/exercicio07.rb
@@ -7,3 +7,16 @@ class NoDuplo
     @prox = prox
   end
 end
+
+def concatenar_lde(comeco_a, comeco_b)
+
+  atual = comeco_a
+  while atual.prox != nil
+    atual = atual.prox
+  end
+
+  atual.prox = comeco_b
+  comeco_b.ant = atual
+
+  comeco_a
+end
