2996354afec45aea4583f0d899eb7b5c9d126238
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 16:29:59 -0300

    feat: cria função para inverte LDE

--- src/exercicio06.rb
+++ src/exercicio06.rb
@@ -7,3 +7,18 @@ class NoDuplo
     @prox = prox
   end
 end
+
+def inverter_lde(comeco)
+  atual = comeco
+  novo_comeco = nil
+
+  while atual != nil
+    proximo = atual.prox
+    atual.prox = atual.ant
+    atual.ant = proximo
+    novo_comeco = atual
+    atual = proximo
+  end
+
+  novo_comeco
+end
