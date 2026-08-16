06b838d9f7dff1ad455c1fe12aa7169eaddd2fe3
VitorVT9 <vsilvab57@gmail.com>
2026-08-16 13:50:00 -0300

    feat: cria lista para comeco_a a comeco_b

--- src/exercicio02.rb
+++ src/exercicio02.rb
@@ -7,17 +7,17 @@ class NoMatricial
   end
 end
 
-lista_2 = Array.new(9)
+lista = Array.new(9)
 
-lista_2[2] = NoMatricial.new("G", 5)
-lista_2[5] = NoMatricial.new("O", 7)
-lista_2[7] = NoMatricial.new("O", 8)
-lista_2[8] = NoMatricial.new("D", 0)
+lista[2] = NoMatricial.new("G", 5)
+lista[5] = NoMatricial.new("O", 7)
+lista[7] = NoMatricial.new("O", 8)
+lista[8] = NoMatricial.new("D", 0)
 
-lista_2[3] = NoMatricial.new("G", 1)
-lista_2[1] = NoMatricial.new("A", 4)
-lista_2[4] = NoMatricial.new("M", 6)
-lista_2[6] = NoMatricial.new("E", 0)
+lista[3] = NoMatricial.new("G", 1)
+lista[1] = NoMatricial.new("A", 4)
+lista[4] = NoMatricial.new("M", 6)
+lista[6] = NoMatricial.new("E", 0)
 
 comeco_a = 2
 comeco_b = 3
